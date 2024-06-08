43b889f938892048b6d8e4a759b7410216c594b6
Aakarsh2207 <aakarsh22sharma007@gmail.com>
2024-06-08 09:59:24 +0530

    Day6 MongoDB-2

---
+++ models/index.js
@@ -0,0 +1,6 @@
+//Main file "index.js" for the 2 particular files "book-model.js" & "user-model.js"     ...where we will have all the Import & Export statements for these 2 files
+const UserModel = require("./user-model");
+const BookModel = require("./book-model");
+
+
+module.exports = {UserModel, BookModel}
